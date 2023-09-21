a6a3433548a25014b1c2e7a9803b08e4272d3e6c
chinnanarajabadge2 <145713615+chinnanarajabadge2@users.noreply.github.com>
2023-09-21 16:37:19 -0400

    Update streamlit_app.py
    Remove test

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit
 streamlit.title('My Parents New Healthy Diner')
-streamlit.header('Arshiya says I love Dad')
+
 streamlit.header('Breakfast Favorites')
 streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
 streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
